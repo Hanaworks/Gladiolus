dcc9b1189ddcbe04cc336c1143e99f1c8f9389c8
Reinakumiko <Reinakumiko@users.noreply.github.com>
2018-09-29 01:22:18 +0800

    添加项目

---
+++ Hanaworks.Gladiolus.Tools/Win32Api.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Hanaworks.Gladiolus.Tools
+{
+    public static class Win32Api
+	{
+		[DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+		public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetConsoleWindow();
+	}
+}
